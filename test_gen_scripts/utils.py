a1cbdb84c004dc6cf273328af08f8e5904356d8c
VerzoToKumpel <verzo.of@gmail.com>
2023-09-06 20:18:22 +0200

    test2

--- test_gen_scripts/utils.py
+++ test_gen_scripts/utils.py
@@ -1,12 +1,9 @@
-import random
-import string
 from hashlib import sha256
 
 from ecdsa import SigningKey, NIST256p, util
 import os
 
-from seed_generator import generate_next_seed
-
+from deterministic_generator import generate_next_seed
 
 def generate_deterministic_key(seed: bytes):
     if seed is not None:
@@ -24,8 +21,6 @@ class DeterministicPRNG:
     def __call__(self, n):
         hash_value = sha256(self.seed).digest()
         return hash_value[:n]
-    
-
 
 def get_good_signature(message: bytes, hash=False):
     sk = generate_deterministic_key(generate_next_seed())
@@ -49,12 +44,6 @@ def get_raw_signature(message: bytes):
     sig = sk.sign(message, hashfunc=sha256)
     (px, py) = (vk.pubkey.point.x(), vk.pubkey.point.y())
     return (sig, px, py)
-
-
-def get_random_string(a: int, b: int):
-    characters = string.ascii_letters + string.digits
-    return "".join(random.choice(characters) for _ in range(random.randint(a, b)))
-
 
 def bytes_as_cairo_array(bytes: bytes, name: str = "msg") -> str:
     declare = [f"let mut {name}: Array<u8> = ArrayTrait::new();"]
