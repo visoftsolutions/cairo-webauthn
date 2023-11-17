7819c331c531b16bb6589f2c7803dd5b359ac34d
Szymon Wojtulewicz <szymon.wojtulewicz@gmail.com>
2023-11-17 13:49:07 +0000

    intermediate, not working :((

--- account_sdk/src/deploy_contract.rs
+++ account_sdk/src/deploy_contract.rs
@@ -1,31 +1,69 @@
 use std::sync::Arc;
 
-use crate::rpc_provider::RpcClientProvider;
+use crate::{rpc_provider::RpcClientProvider, deployer::{Deployable, Declarable}};
 use starknet::{
-    accounts::{Account, ExecutionEncoding, SingleOwnerAccount},
+    accounts::{Account, Call, ExecutionEncoding, SingleOwnerAccount},
     core::types::{
         contract::{CompiledClass, SierraClass},
-        DeclareTransactionResult, FieldElement, BlockId, BlockTag,
+        BlockId, BlockTag, DeclareTransactionResult, FieldElement, InvokeTransactionResult,
     },
+    macros::selector,
     providers::{JsonRpcClient, Provider},
-    signers::{LocalWallet, SigningKey},
+    signers::{LocalWallet, Signer, SigningKey},
 };
 
-const SIERRA_STR: &str =
-    include_str!("../../cartridge_account/target/dev/cartridge_account_Account.contract_class.json");
-const CASM_STR: &str =
-    include_str!("../../cartridge_account/target/dev/cartridge_account_Account.compiled_contract_class.json");
+const SIERRA_STR: &str = include_str!(
+    "../../cartridge_account/target/dev/cartridge_account_Account.contract_class.json"
+);
+const CASM_STR: &str = include_str!(
+    "../../cartridge_account/target/dev/cartridge_account_Account.compiled_contract_class.json"
+);
 
-pub async fn deploy_contract<T>(
+pub struct CustomContract;
+
+impl Declarable for CustomContract {
+    fn artifact_str(&self) ->  &str {
+        SIERRA_STR
+    }
+}
+
+impl Deployable for CustomContract {
+    fn salt(&self) -> FieldElement {
+        FieldElement::default()
+    }
+}
+
+pub async fn declare_and_deploy_contract<T>(
     rpc_provider: impl RpcClientProvider<T>,
     signing_key: SigningKey,
     address: FieldElement,
-) -> Result<DeclareTransactionResult, String>
+    constructor_calldata: Vec<FieldElement>,
+) -> Result<InvokeTransactionResult, String>
 where
     JsonRpcClient<T>: Provider,
     T: Send + Sync,
 {
-    let _ = rpc_provider;
+    let (result, account) = declare_contract(rpc_provider, signing_key, address)
+        .await
+        .unwrap();
+    deploy_contract(address, constructor_calldata, account, result.class_hash).await
+}
+
+pub async fn declare_contract<T>(
+    rpc_provider: impl RpcClientProvider<T>,
+    signing_key: SigningKey,
+    address: FieldElement,
+) -> Result<
+    (
+        DeclareTransactionResult,
+        SingleOwnerAccount<JsonRpcClient<T>, LocalWallet>,
+    ),
+    String,
+>
+where
+    JsonRpcClient<T>: Provider,
+    T: Send + Sync,
+{
     // Sierra class artifact. Output of the `starknet-compile` command
     let contract_artifact: SierraClass =
         serde_json::from_str(SIERRA_STR).map_err(|e| e.to_string())?;
@@ -35,6 +73,67 @@ where
         serde_json::from_str(CASM_STR).map_err(|e| e.to_string())?;
     let compiled_class_hash = compiled_class.class_hash().map_err(|e| e.to_string())?;
 
+    let account = get_account(rpc_provider, signing_key, address);
+
+    // We need to flatten the ABI into a string first
+    let flattened_class = contract_artifact.flatten().map_err(|e| e.to_string())?;
+
+    let declaration = account.declare(Arc::new(flattened_class), compiled_class_hash);
+
+    Ok(declaration
+        .send()
+        .await
+        .map(|r| {
+            std::thread::sleep(std::time::Duration::from_secs(10));
+            (r, account)
+        })
+        .unwrap())
+}
+
+pub async fn deploy_contract<P, S>(
+    address: FieldElement,
+    constructor_calldata: Vec<FieldElement>,
+    account: SingleOwnerAccount<P, S>,
+    class_hash: FieldElement,
+) -> Result<InvokeTransactionResult, String>
+where
+    P: Provider + Send + Sync,
+    S: Signer + Send + Sync,
+{
+    let calldata = [
+        vec![
+            class_hash,
+            FieldElement::from_hex_be("0x1").unwrap(), // salt
+            FieldElement::ZERO,                        // unique
+            FieldElement::from(constructor_calldata.len()),
+        ],
+        constructor_calldata,
+    ]
+    .concat();
+
+    Ok(account
+        .execute(vec![Call {
+            calldata,
+            selector: selector!("deployContract"),
+            to: address,
+        }])
+        .send()
+        .await
+        .map(|v| {
+            std::thread::sleep(std::time::Duration::from_secs(10));
+            v
+        }).unwrap())
+}
+
+pub fn get_account<T>(
+    rpc_provider: impl RpcClientProvider<T>,
+    signing_key: SigningKey,
+    address: FieldElement,
+) -> SingleOwnerAccount<JsonRpcClient<T>, LocalWallet>
+where
+    JsonRpcClient<T>: Provider,
+    T: Send + Sync,
+{
     let client = rpc_provider.get_client();
     let signer = LocalWallet::from(signing_key);
 
@@ -49,11 +148,5 @@ where
     // `SingleOwnerAccount` defaults to checking nonce and estimating fees against the latest
     // block. Optionally change the target block to pending with the following line:
     account.set_block_id(BlockId::Tag(BlockTag::Pending));
-
-    // We need to flatten the ABI into a string first
-    let flattened_class = contract_artifact.flatten().map_err(|e| e.to_string())?;
-
-    let declaration = account.declare(Arc::new(flattened_class), compiled_class_hash);
-
-    declaration.send().await.map_err(|e| e.to_string())
+    account
 }
